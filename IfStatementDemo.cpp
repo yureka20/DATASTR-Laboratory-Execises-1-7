4dd1395e7a5c91f796eb20ad6ce76da5a762e1df
villanuevamarmico <99540783+yureka20@users.noreply.github.com>
2025-10-29 23:50:43 +0800

    Add files via upload

---
+++ IfStatementDemo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0)
+        cout << "The number is positive." << endl;
+    else
+        cout << "The number is zero or negative." << endl;
+
+    return 0;
+}
